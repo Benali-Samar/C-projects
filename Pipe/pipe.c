47e88d0166d7059ae0be4c6228d9bf14122cb5d5
Ben Ali samar <samar.benali@istic.ucar.tn>
2023-05-08 17:26:25 +0100

    Child pipe

--- Pipe/pipe.c
+++ Pipe/pipe.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
 
 int main(void) {
 	int pipefds[2];    //creating a pipes
@@ -12,17 +15,37 @@ int main(void) {
 		exit(EXIT_FAILURE);
 	}
 
+	pid_t pid = fork(); // creates child process
+	if (pid == 0)
+	{
+	close(pipefds[0]);
+	write (pipefds[1],p,5); //writes p to pipe
+
+	printf("Sending p from child to parent ...\n");
+	sleep(2);
+	exit (EXIT_SUCCESS);
+	}
 
 
-	printf("Writing p to pipe ... \n");
-	write(pipefds[1],p,5); //it writes the 5  characters to the pipe
-	printf("Done writing \n\n");
+	if (pid > 0)
+	{
+	wait (NULL);
+	close(pipefds[1]);
+	read(pipefds[0],buf,5); //read p from pipe
+	close(pipefds[0]);
 
-	printf("Reading p from pip ... \n");
-	read(pipefds[0],buf,5); // reads them from the pip  and for displaying using buffer
-	printf("Done reading \n\n");
+	printf("Parent recieving p '%s' \n", buf);
+	}
 
-	printf("P from pipe : %s \n", buf);
+//	printf("Writing p to pipe ... \n");
+//	write(pipefds[1],p,5); //it writes the 5  characters to the pipe
+//	printf("Done writing \n\n");
+
+//	printf("Reading p from pip ... \n");
+//	read(pipefds[0],buf,5); // reads them from the pip  and for displaying using buffer
+//	printf("Done reading \n\n");
+
+//	printf("P from pipe : %s \n", buf);
 
 //	printf("Read fd value : %d \n", pipefds[0]);
 //	printf("Write fd value : %d \n", pipefds[1]);
