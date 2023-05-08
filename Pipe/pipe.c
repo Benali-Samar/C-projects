f7a75a91ab2dfd2ff396e435eb28df5945762cdd
Ben Ali samar <samar.benali@istic.ucar.tn>
2023-05-08 17:47:33 +0100

    Continously creats child and send

--- Pipe/pipe.c
+++ Pipe/pipe.c
@@ -4,25 +4,41 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 
+#define P_LENGTH 4
+#define P_WAIT_INTERVAL 2
+
+// continously creates a child process and generates p in it and sends the p to the parent using pipe
+void getP(char p[P_LENGTH +1])
+{
+	srand(getpid() + getppid());
+	p[0] = 49 + rand() %7;
+
+	for (int i = 1; i< P_LENGTH; i++)
+	{
+	p[i] = 48 + rand() % 7;
+	}
+	p[P_LENGTH] = '\0';
+}
+
 
 int main(void) {
+	while(1)
+	{
 	int pipefds[2];    //creating a pipes
-	char buf[5];       // buffer for the reading
-	char *p="1234\0";  //explicity added the null character cz while writing it to the buff by pipe it should know the end of the char
+	char buf[P_LENGTH +1];       // buffer for the reading
+	char p[P_LENGTH + 1 ] ;
 
-	if (pipe(pipefds) == -1){
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
+	pipe(pipefds);
 
 	pid_t pid = fork(); // creates child process
 	if (pid == 0)
 	{
+	getP(p);
 	close(pipefds[0]);
-	write (pipefds[1],p,5); //writes p to pipe
+	write (pipefds[1],p, P_LENGTH +1 ); //writes p to pipe
 
 	printf("Sending p from child to parent ...\n");
-	sleep(2);
+	sleep(P_WAIT_INTERVAL);
 	exit (EXIT_SUCCESS);
 	}
 
@@ -31,12 +47,12 @@ int main(void) {
 	{
 	wait (NULL);
 	close(pipefds[1]);
-	read(pipefds[0],buf,5); //read p from pipe
+	read(pipefds[0],buf,P_LENGTH + 1); //read p from pipe
 	close(pipefds[0]);
 
 	printf("Parent recieving p '%s' \n", buf);
 	}
-
+	}
 //	printf("Writing p to pipe ... \n");
 //	write(pipefds[1],p,5); //it writes the 5  characters to the pipe
 //	printf("Done writing \n\n");
