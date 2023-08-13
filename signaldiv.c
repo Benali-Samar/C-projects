503d467f2ed9bcf832b1013118fa0db1439fce8e
Benali-Samar <samar.benali@istic.ucar.tn>
2023-08-13 23:20:35 +0100

    jump signal with context registration

--- signaldiv.c
+++ signaldiv.c
@@ -11,11 +11,19 @@ sigjmp_buf env;
 
 void manager (int number)
 {
+	static int trys = 1;
 	if (number == SIGFPE)
 	{
+		if(trys == 5)
+		{
+			printf("You already used your trys.\n");
+			exit(3);
+		}
+
 		printf("Division with zero is detected...\n");
 		printf("Enter a non zero number: ");
 		scanf("%d", &y);
+		trys ++;
 
 		//Execution contexte restutition (return value)
 		siglongjmp(env,1);
@@ -37,7 +45,16 @@ int main(void)
 	printf("Enter two numbers x and y : \n");
 	scanf("%d %d", &x, &y);
 
-	sigsetjmp (env, 1); //Contexte registration
+
+	int r = sigsetjmp (env, 1); //Contexte registration
+	// if r == 0: we reached here normally
+	// if r != 0: we jumped in code with (siglongjmp)
+	// r value is specified  in the siglongjmp call.
+
+	if (r !=0)
+	{
+		printf("Try %d of division ... \n", r);
+	}
 
 	z= x / y;
 	printf("%d/%d = %d\n", x, y, z);
