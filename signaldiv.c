19eba879cd3ed1b615ffa82dcc4f000b993bbe0d
Benali-Samar <samar.benali@istic.ucar.tn>
2023-08-13 23:09:01 +0100

    contexte registration signal

--- signaldiv.c
+++ signaldiv.c
@@ -2,15 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+// for the contexte restitution
+#include <setjmp.h>
+
 
 int x, y, z;
+sigjmp_buf env;
 
 void manager (int number)
 {
 	if (number == SIGFPE)
 	{
 		printf("Division with zero is detected...\n");
-		exit(1);
+		printf("Enter a non zero number: ");
+		scanf("%d", &y);
+
+		//Execution contexte restutition (return value)
+		siglongjmp(env,1);
 	}
 }
 
@@ -28,6 +36,9 @@ int main(void)
 	}
 	printf("Enter two numbers x and y : \n");
 	scanf("%d %d", &x, &y);
+
+	sigsetjmp (env, 1); //Contexte registration
+
 	z= x / y;
 	printf("%d/%d = %d\n", x, y, z);
 	return 0;
